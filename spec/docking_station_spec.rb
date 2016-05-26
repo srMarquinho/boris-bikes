078a70a040c1389c397426e351cd813bcfc42bde
Lukasz <lukasz.grychowski@gmail.com>
2016-05-26 11:44:18 +0100

    day 3 start

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,13 +6,12 @@ describe DockingStation do
 	end
 
 	it 'releases a working bike' do
-
     bike = Bike.new
 		expect(bike).to be_working
 	end
 
 	it 'responds to dock' do
-		expect(subject).to respond_to :dock
+	  expect(subject).to respond_to :dock
 	end
 
 	it 'accepts an argument' do
@@ -20,23 +19,23 @@ describe DockingStation do
 	end
 
   it 'docks a bike' do
-  expect(subject.dock(Bike.new)).to eq subject.bikes
+    expect(subject.dock(Bike.new)).to eq subject.bikes
   end
 
-   it "has a docked bike" do
+  it "has a docked bike" do
      expect(subject).to respond_to :bikes
-   end
-
-  describe '#release_bike' do
-   it 'doesnt return a bike if empty' do
-     expect {subject.release_bike}.to raise_error("We have no bikes!")
-   end
   end
 
-  describe '#dock' do
-    it 'cannot return bike if full' do
-      20.times{ subject.dock(Bike.new)}
-      expect {subject.dock(Bike.new)}.to raise_error("We have no space!")
+    describe '#release_bike' do
+      it 'doesnt return a bike if empty' do
+        expect {subject.release_bike}.to raise_error("We have no bikes!")
+      end
     end
-  end
+
+    describe '#dock' do
+      it 'cannot return bike if full' do
+        20.times{ subject.dock(Bike.new)}
+        expect {subject.dock(Bike.new)}.to raise_error("We have no space!")
+      end
+    end
 end
